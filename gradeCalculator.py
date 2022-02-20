1caf5a8215ff421b8e97ddab762e788088a3bff1
Brett McElvain <brett@Bretts-MacBook-Pro.local>
2022-02-20 12:40:04 -0600

    Added error checking for when user enters non-numericals.

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -5,21 +5,24 @@ def calculateGrade():
     # Implement your solution in between the two comment blocks
     print("Calculating Grade")
     # This first line is provided for you
-
-    score = float(input("Enter score:"))
-    if score <= 1.0 and score >= 0.9:
-        grade = "A"
-    elif score < 0.9 and score >= 0.8:
-        grade = "B"
-    elif score < 0.8 and score >= 0.7:
-        grade = "C"
-    elif score < 0.7 and score >= 0.6:
-        grade = "D" 
-    elif score < 0.6 and score >= 0.0:
-        grade = "F" 
-    else:
+    try:
+        score = float(input("Enter score:"))
+        if score <= 1.0 and score >= 0.9:
+            grade = "A"
+        elif score < 0.9 and score >= 0.8:
+            grade = "B"
+        elif score < 0.8 and score >= 0.7:
+            grade = "C"
+        elif score < 0.7 and score >= 0.6:
+            grade = "D" 
+        elif score < 0.6 and score >= 0.0:
+            grade = "F" 
+        else:
+            grade = "Bad score"
+        print(grade)
+    except:
         grade = "Bad score"
-    print(grade)
+        print(grade)
     # end assignment
 
 ## if you want to test locally before you try to sync
