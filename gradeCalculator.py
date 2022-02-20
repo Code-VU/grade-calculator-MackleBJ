b85d246197e7181a8e6f8def8e6ae78f2be1b8c0
Brett McElvain <brett@Bretts-MacBook-Pro.local>
2022-02-20 12:32:33 -0600

    Created calculateGrade() function.

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,10 +1,25 @@
+from re import A
+
+
 def calculateGrade():
     # Implement your solution in between the two comment blocks
     print("Calculating Grade")
     # This first line is provided for you
 
-    hrs = float(input("Enter score:"))
-
+    score = float(input("Enter score:"))
+    if score >= 0.9:
+        grade = "A"
+    elif score < 0.9 and score >= 0.8:
+        grade = "B"
+    elif score < 0.8 and score >= 0.7:
+        grade = "C"
+    elif score < 0.7 and score >= 0.6:
+        grade = "D" 
+    elif score < 0.6:
+        grade = "F" 
+    else:
+        grade = "Bad score"
+    print(grade)
     # end assignment
 
 ## if you want to test locally before you try to sync
